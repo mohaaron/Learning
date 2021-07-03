c27ed557271ffb5af1ae857c3798396d49a8b070
Aaron <mohaaron@gmail.com>
2021-07-03 18:05:46 -0400

    Move inline code to code-behind (#8)
    * Replace HttpClient with repository interface (#2)
    
    Replace HttpClient injection in razor components with the use of repository interface and injection in codebehind.
    
    * Move inline code blocks to codebehind #6

---
+++ BlazorAppSln/BlazorApp.Data.Http.Repositories/BudgetHttpRepository.cs
@@ -0,0 +1,38 @@
+﻿using BlazorApp.Data.Models;
+using BlazorApp.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data.Http.Repositories
+{
+	public class BudgetHttpRepository : IBudgetRepository
+	{
+		private readonly HttpClient httpClient;
+
+		public BudgetHttpRepository(HttpClient httpClient)
+		{
+			this.httpClient = httpClient;
+		}
+
+		public async Task<Budget> GetBudget(int id)
+		{
+			return await httpClient.GetFromJsonAsync<Budget>("api/Budget/" + id);
+		}
+
+		public async Task<DbTaskResult> SaveBudget(Budget budget)
+		{
+			var resp = await httpClient.PutAsJsonAsync<Budget>("api/Budget", budget);
+			return new DbTaskResult
+			{
+				StatusCode = resp.StatusCode
+			};
+		}
+	}
+}
