1d1674a2b9b767657a03af702fa433c90d92672a
Aaron Prohaska <mohaaron@gmail.com>
2021-09-07 16:39:45 -0400

    Add service layer project (#29).

---
+++ BlazorAppSln/BlazorApp.Services/BudgetService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp.Data.Models;
+using BlazorApp.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Services
+{
+	public class BudgetService : IBudgetService
+	{
+		private IBudgetRepository repository;
+
+		public BudgetService(IBudgetRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		public Task<Budget> Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
