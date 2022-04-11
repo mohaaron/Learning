180ef17c74ea2881baa6de6153ec414ffde537b3
Aaron Prohaska <mohaaron@gmail.com>
2022-04-11 10:08:22 -0700

    Add AutoMaper for cloning models (#44)

---
+++ BlazorAppSln/BlazorApp/Shared/AutoMapper/ExpenseProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BlazorApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.AutoMapper
+{
+    public class ExpenseProfile : Profile
+    {
+        public ExpenseProfile()
+        {
+            // TODO: Options.Ignore() doesn't work. Find out why
+            CreateMap<Expense, Expense>()
+                .ForMember(dest => dest.Budget, opt => opt.Condition(source => source.Budget == null))
+                .ForMember(dest => dest.Income, opt => opt.Condition(source => source.Income == null));
+        }
+    }
+}
---
+++ BlazorAppSln/BlazorApp/Shared/AutoMapper/IncomeProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BlazorApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.AutoMapper
+{
+    public class IncomeProfile : Profile
+    {
+        public IncomeProfile()
+        {
+            // TODO: Options.Ignore() doesn't work. Find out why
+            CreateMap<Income, Income>()
+                .ForMember(dest => dest.Budget, opt => opt.Condition(source => source.Budget == null))
+                .ForMember(dest => dest.Expenses, opt => opt.Condition(source => source.Expenses == null));
+        }
+    }
+}
